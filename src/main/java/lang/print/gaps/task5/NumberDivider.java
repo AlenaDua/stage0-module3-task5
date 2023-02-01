c32cacc1fa0da7806a5249662a5017fa03edfbca
Alena Dubadzelava <dubodelova.minsk@gmail.com>
2023-02-01 13:12:37 +0100

    done

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task5;
 
 public class NumberDivider {
-    void divide(int toBeDivided) {
+    static void divide(int toBeDivided) {
+        System.out.printf("%.1f\n",(double)toBeDivided/5);
+    }
+
+    public static void main(String[] args) {
+        int toBeDivided1 = 0;
+        divide(toBeDivided1);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,16 @@
 package lang.print.gaps.task5;
 
 public class TimeConvertor {
-    public void convert(float minutes) {
+    public static void convert(float minutes) {
+        double sekonds = 60 * minutes;
+        System.out.println(sekonds);
+
+    }
+
+    public static void main(String[] args) {
+        float minutes = 0;
+        convert(minutes);
+
     }
 }
+
